b902843f8e8eb55120a88773bf0de6903a466552
Zahra Khan <zarssskhan@gmail.com>
2017-09-22 04:11:27 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,7 +23,7 @@ function submitIt() {
   $('form').on('submit', function() {
     alert("Your form is going to be submitted now.")
   })
-}
+};
 
 
 $(document).ready(function(){
@@ -32,5 +32,5 @@ $(document).ready(function(){
 getIt();
 frameIt();
 pressIt();
-
+submitIt();
 });
