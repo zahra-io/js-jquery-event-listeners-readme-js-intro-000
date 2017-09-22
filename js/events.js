f88d246ebe057fa16aea0f61f5989a7e685ac898
Zahra Khan <zarssskhan@gmail.com>
2017-09-22 04:09:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 };
 
 function pressIt() {
-  $(document).on('keydown', function(key) {
+  $('#typing').on('keydown', function(key) {
     if(key.which === 71) {
       alert("You have pressed G!")
     }
