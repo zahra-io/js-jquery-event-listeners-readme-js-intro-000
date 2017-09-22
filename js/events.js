6fbb50bc1c1692734b351bddba92332aa2090d07
Zahra Khan <zarssskhan@gmail.com>
2017-09-22 04:11:49 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  })
+};
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty');
+  })
+};
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which === 71) {
+      alert("You have pressed G!")
+    }
+  })
+};
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+  })
+};
+
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
